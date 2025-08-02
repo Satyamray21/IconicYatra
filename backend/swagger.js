7a45ff65156c4c3aa45cc3fb083dd26bff2c4f53
Satyamray21 <satyamray43@gmail.com>
2025-08-02 12:48:38 +0530

    added swagger

---
+++ backend/swagger.js
@@ -0,0 +1,34 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  swagger: "2.0",
+  info: {
+    title: 'Iconic Yatra',
+    description: 'Auto-generated Swagger docs',
+    version: '1.0.0',
+  },
+  host: 'localhost:4000',
+  basePath: '/',
+  schemes: ['http'],
+  consumes: ['application/json', 'multipart/form-data'],
+  produces: ['application/json'],
+  securityDefinitions: {
+    bearerAuth: {
+      type: 'apiKey',
+      name: 'Authorization',
+      in: 'header',
+      description: 'Enter your JWT token in the format: Bearer <token>',
+    },
+  },
+  security: [
+    {
+      bearerAuth: [],
+    },
+  ],
+ 
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./app.js'];
+
+swaggerAutogen()(outputFile, endpointsFiles, doc);
