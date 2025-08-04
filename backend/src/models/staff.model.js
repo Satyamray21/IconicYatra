923fc14adad52a5ad82ac155d16bb1c8e8686601
Satyamray21 <satyamray43@gmail.com>
2025-08-04 15:21:28 +0530

    added staff model.

---
+++ backend/src/models/staff.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose"
+import { addressSchema } from "../common/address.common";
+import { firmSchema } from "../common/firm.common";
+import { bankSchema } from "../common/bankDetails.common";
+const staffSchema = new mongoose.Schema({
+    personalDetails :{
+        firstName:{
+            type:String,
+            required:true
+        },
+        lastName:{
+            type:String,
+            required:true
+        },
+        mobileNumber:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        alternateContact:{
+            type:String,
+            unique:true
+        },
+        designation:{
+            type:String,
+            required:true
+        },
+        userRole:{
+            type:String,
+            enum:[Superadmin,Admin,executive],
+            required:true
+        }
+
+    },
+    staffLocation:{
+    country: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Country",
+      required: true
+    },
+    state: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "State",
+      required: true
+    },
+    city: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "City",
+      required: true
+    }
+},
+address:addressSchema,
+firm:firmSchema,
+bank:bankSchema,
+},{timestamps:true});        
+export const Staff = new mongoose.model(Staff,"staffSchema");
