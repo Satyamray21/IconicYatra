2f072d9707e6cd15a692a5beee23190e785f2859
Satyamray21 <satyamray43@gmail.com>
2025-08-04 15:20:56 +0530

    aaded index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
-import dotenv from "dotenv"
-import connectDB from "./src/DB/index.js"
-import { app } from "./app.js"
+import dotenv from "dotenv";
+import connectDB from "./src/db/index.js"
+import {app} from "./app.js"
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the SK SAHU JAIN APi!');
+});
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 3000, () => {
             console.log(`server is running  on ${process.env.PORT}`)
         })
     })
-    .catch(() => {
-        console.log("Server failed");
+    .catch((err) => {
+        console.log("Server failed",err);
     })
