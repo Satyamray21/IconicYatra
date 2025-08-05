c5caf1b5d186de2fcab75333ffb16c66f6f536a4
Satyamray21 <satyamray43@gmail.com>
2025-08-05 10:23:31 +0530

    added reducer for staff.

--- dashboard/src/store/store.js
+++ dashboard/src/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import LeadReducer from "../features/leads/leadSlice"
+import StaffReducer from "../features/staff/staffSlice"
 export const store = configureStore({
   reducer: {
     leads:LeadReducer,
+    staffs:StaffReducer,
+
   },
 });
 
